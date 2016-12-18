a8eebb62339ed12cee6b9229369d8dc9481d436f
Ildiko Czeller <czeildi@gmail.com>
2016-12-18 21:01:48 +0100

    test with defaultPort = 2016

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-var defaultPort = 3000
+var defaultPort = 2016
 
 app.listen(defaultPort, function () {
   console.log('word-associations app listening on port ' + defaultPort)
