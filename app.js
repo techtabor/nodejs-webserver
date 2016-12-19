ea3b54a15501631887c0f9595a3841b522e435c0
Ildiko Czeller <czeildi@gmail.com>
2016-12-19 19:59:09 +0100

    restore initial state for cloning

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
-require('./router/main')(app)
-
-var defaultPort = 3000
-
-app.listen(defaultPort, function () {
-  console.log('word-associations listening on port ' + defaultPort)
-})
--- router/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (app) {
-  app.get('/title', function (req, res) {
-    res.send('word-associations')
-  })
-
-  app.get('/words', function (req, res) {
-    res.send('words:')
-  })
-}
